796bff336cf4046a684f953e66ae11e7e0a99f16
Anton Tarakanov <anniktar11@gmail.com>
2022-11-26 00:40:55 +0500

    small fix

---
+++ src/library/renderConstants.js
@@ -0,0 +1,13 @@
+export const COMMON_CLASS_NAMES = {
+    APP: 'application',
+    FLEX: 'flex',
+    JUSTIFY_CENTER: 'justifyCenter',
+    HEADER: 'header',
+    HEADER_IMG: 'header_img',
+}
+
+export const COMMON_DOM_IDS = {
+    MAIN: 'mainField',
+    ADDITIONAL: 'additionalField',
+    HEADER: 'header',
+}
